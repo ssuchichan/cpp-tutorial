af343ad3ee0aa73946b3d93522f222c100292c0d
蝌蚪游长江 <tadpoleswiminyangtze@gmail.com>
2024-04-23 23:56:25 -0700

    operator overload. (#13)

--- day05/10complex/main.cpp
+++ day05/10complex/main.cpp
@@ -15,10 +15,22 @@ public:
         return *this;
     }
 
+    const Complex operator++(int) {
+        Complex old = *this;
+        ++*this;
+        return old;
+    }
+
     friend Complex& operator--(Complex& o) {
         --o.m_r;
         --o.m_i;
         return o;
+    }
+
+    friend const Complex operator--(Complex& o, int) {
+        Complex old = o;
+        --o;
+        return old;
     }
 
 private:
@@ -31,8 +43,17 @@ int main() {
     Complex c2 = ++++c1; // c1.operator++().operator++();
     c1.print(); // 3 + 4i
     c2.print(); // 3 + 4i
+
     c2 = ----c1;
     c2.print(); // 1 + 2i
+
+    c2 = c1++; // c2 = c1.operator++(0)
+    c1.print(); // 2 + 3i
+    c2.print(); // 1 + 2i
+
+    c2 = c1--;
+    c1.print(); // 1 + 2i
+    c2.print(); // 2 + 3i
 
     return 0;
 }
